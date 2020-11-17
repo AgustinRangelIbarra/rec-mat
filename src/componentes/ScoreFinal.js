3bc95b3343aea534bcec9bde6098141a83abd4f5
Agustin Rangel <agustinran.ib@gmail.com>
2020-11-17 03:16:19 -0600

    Estilo Prueba - ScoreFinal.js

--- src/componentes/ScoreFinal.js
+++ src/componentes/ScoreFinal.js
@@ -1,9 +1,9 @@
 export default function ScoreFinal({ score }) {
   return (
-    <div className="row py-5">
-      <div className="col-12 text-center">
-        <div className="h3">Tu puntación final es de: {score}</div>
+      <div className="row py-5">
+        <div className="col-12 text-center">
+          <div className="h4 alert alert-info">Tu puntación final es de: {score}</div>
+        </div>
       </div>
-    </div>
   );
 }
