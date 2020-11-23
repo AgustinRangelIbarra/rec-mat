fc0594d7540615226ccdecd65c4d44d9cc613192
Agustin Rangel <agustinran.ib@gmail.com>
2020-11-23 11:35:09 -0600

    footer

---
+++ src/componentes/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+	return ( 
+		<div>
+			<footer className="flex-shrink-0 py-1  text-white-50">
+				<div className="container text-center">
+					<small>Instituto Politecnico Nacional</small>
+				</div>
+			</footer>
+		</div>
+	);
+}
+
+export default Footer;
