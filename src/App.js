6ec3dc6f332e2744120e8f92d7fab5f69b5c19d2
Agustin Rangel <agustinran.ib@gmail.com>
2021-01-24 16:51:37 -0600

    BrowserRouter por HashRouter

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import $ from "jquery";
 import Popper from "popper.js";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 // Llamada de los Componentes
 
